ba2b144a8327732d36debdaf5c25e091baf3d6d2
Oded Arbel <oded@geek.co.il>
2016-04-02 19:18:57 +0300

    more logging

--- app/scripts/controll.js
+++ app/scripts/controll.js
@@ -232,6 +232,7 @@ var ConTroll = (function(w,d){
 		}, function(res, err) {
 			if (err) {
 				console.log('Error', err.error || err);
+				alert("Error: " + (err.error || err));
 				return;
 			}
 			callback(res);
